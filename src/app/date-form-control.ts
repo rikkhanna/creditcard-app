21e4e8f69ddda4bb77498711776706d0ad43c3dc
Rishabh Khanna <rishabhkhanna726@gmail.com>
2020-07-09 22:41:33 +0530

    input masking form date field

--- src/app/date-form-control.ts
+++ src/app/date-form-control.ts
@@ -3,7 +3,10 @@ import { FormControl } from '@angular/forms';
 export class DateFormControl extends FormControl {
 
     setValue(value:string, options: any){
-        console.log(value);
-        super.setValue(value,options)
+        if(value.length == 2){
+            super.setValue(value + '/',{...options, emitModelToViewChange: true})
+            return;
+        }
+        super.setValue(value, {...options, emitModelToViewChange: true})
     }
 }
